157879ead8fba1a7f13d0cfc9f18cb8f5e85dd81
Sean <sdgerbich@gmail.com>
2018-12-01 17:09:46 -0500

    fixed css

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var PORT = 3000;
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(express.static(__dirname + '/public/'));
+app.use(express.static(__dirname + '/public/assets/html/'));
 require("./routes/htmlRoutes")(app);
 
 app.listen(PORT, function() {
