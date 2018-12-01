636414b61e6b16ccd13e5c80f9679b9aa0efad13
Sean <sdgerbich@gmail.com>
2018-12-01 17:59:56 -0500

    fixed port

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var path = require("path");
 var app = express();
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;;
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
